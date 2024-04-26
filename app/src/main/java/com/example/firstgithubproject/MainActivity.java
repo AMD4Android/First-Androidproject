16914eec005606a8cc6acf101c2e721ba4d1621b
Ahmed <alariqi.aa@gmail.com>
2024-04-26 09:48:45 +0200

    first change

---
+++ app/src/main/java/com/example/firstgithubproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.firstgithubproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Change1
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
